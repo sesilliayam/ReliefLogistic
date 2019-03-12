50b4ada5cc1dde16a93a224fd5342ece7f2b10e8
amelliabriliant <amellia_briliant@yahoo.com>
2019-03-12 18:11:18 +0700

    test FAQ

--- app/src/main/java/com/delisar/relo/MainActivity.java
+++ app/src/main/java/com/delisar/relo/MainActivity.java
@@ -2,12 +2,35 @@ package com.delisar.relo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private FAQAdapter adapter;
+    private ArrayList<QnA> FAQArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addData();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        adapter = new FAQAdapter(FAQArrayList);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void addData() {
+        FAQArrayList = new ArrayList<>();
+        FAQArrayList.add(new QnA("Q : Bagaimana Berdonasi dengan RELO", "A: Donasi dapat dilakukan dengan langsung mengirim Donasi kealamat yang telah di tentukan saat melakukan Check Out di Relo"));
+        FAQArrayList.add(new QnA("Q : Apakah bisa berdonasi dengan Uang di RELO", "A: Saat ini donasi yang diterima RELo hanyalah barang"));
     }
 }
