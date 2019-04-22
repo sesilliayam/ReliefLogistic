c6faa3e36b747b83473d224d33b962e627c6d8e0
liemagung <liemagung1@gmail.com>
2019-04-23 00:50:12 +0700

    fixing integration to forgot pasword

--- app/src/main/java/com/delisar/relo/ForgotPassword.java
+++ app/src/main/java/com/delisar/relo/ForgotPassword.java
@@ -1,7 +1,9 @@
 package com.delisar.relo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_forgot_password );
+    }
+
+    public void toLoginfromForgot(View view){
+        startActivity ( new Intent ( ForgotPassword.this, LoginActivity.class ) );
+        finish ();
     }
 }
