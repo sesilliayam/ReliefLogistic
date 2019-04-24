c060c0167b970f3089aecdf8acf58089d6240abe
liemagung <liemagung1@gmail.com>
2019-04-24 20:08:42 +0700

    udating category module

--- app/src/main/java/com/delisar/relo/Category/Category.java
+++ app/src/main/java/com/delisar/relo/Category/Category.java
@@ -1,4 +1,30 @@
 package com.delisar.relo.Category;
 
 public class Category {
+
+    private String title;
+    private String info;
+//    private final int imageResource;
+
+
+    public Category(String title, String info) {
+        this.title = title;
+        this.info = info;
+//        this.imageResource = imageResource;
+    }
+
+
+    String getTitle() {
+        return title;
+    }
+
+
+    String getInfo() {
+        return info;
+    }
+
+//    public int getImageResource() {
+//        return imageResource;
+//    }
+
 }
--- app/src/main/java/com/delisar/relo/Category/CategoryMain.java
+++ app/src/main/java/com/delisar/relo/Category/CategoryMain.java
@@ -2,14 +2,32 @@ package com.delisar.relo.Category;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 
 import com.delisar.relo.R;
 
+import java.util.ArrayList;
+
 public class CategoryMain extends AppCompatActivity {
+
+    //deklarasi komponen
+    RecyclerView recyclerViewCategory;
+    private ArrayList<Category> listCategory;
+    private ListAdapter listAdapter;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_category_main );
+
+        //inisialisasi komponen
+        recyclerViewCategory = findViewById ( R.id.recyclerCategory );
+
+        //mengisi list kategori
+        listCategory = new ArrayList<> (  );
+
     }
 }
