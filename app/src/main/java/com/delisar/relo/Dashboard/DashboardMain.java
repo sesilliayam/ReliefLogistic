c60998d77dc62617cba358f6d415310914afb466
sesilliayam <sesilliafajar@gmail.com>
2019-04-26 18:54:15 +0700

    fixing layout and Menu Dashboard

--- app/src/main/java/com/delisar/relo/Dashboard/DashboardMain.java
+++ app/src/main/java/com/delisar/relo/Dashboard/DashboardMain.java
@@ -127,29 +127,28 @@ public class DashboardMain extends AppCompatActivity implements View.OnClickList
                 animateFAB();
                 break;
             case R.id.fab1:
-                //profile
-                Intent intent = new Intent (DashboardMain.this, ImageActivity.class);
-                startActivity(intent);
-                Log.d("FAB1", "Fab 1");
-                break;
-            case R.id.fab2:
                 //community
                 Intent intentCommunity = new Intent (DashboardMain.this, CommunityMain.class);
                 startActivity(intentCommunity);
+                Log.d("FAB1", "Fab 1");
+                break;
+            case R.id.fab2:
+                //category
+                startActivity ( new Intent ( DashboardMain.this, CategoryMain.class ) );
                 Log.d("FAB2", "Fab 2");
                 break;
             case R.id.fab3:
-                //category
-                startActivity ( new Intent ( DashboardMain.this, CategoryMain.class ) );
+                //history
                 Log.d("FAB3", "Fab 3");
                 break;
             case R.id.fab4:
-                Log.d("FAB4", "Fab 4");
-                break;
-            case R.id.fab5:
                 //faq
                 Intent intentFAQ = new Intent (DashboardMain.this, FAQMain.class);
                 startActivity(intentFAQ);
+                Log.d("FAB4", "Fab 4");
+                break;
+            case R.id.fab5:
+                //contact us
                 Log.d("FAB5", "Fab 5");
                 break;
         }
